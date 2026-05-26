17d524245cf2641a97c11bc0e1b07f0fc11b68da
Sérgio Timoteo <74915883+sergiotim@users.noreply.github.com>
2026-05-26 19:16:44 -0300

    feat: update GitHub Actions workflow and Next.js configuration for improved deployment

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  // Necessário para o GitHub Pages quando não é um domínio customizado na raiz
   basePath: '/renomeador',
-  // Recomendado para exportação estática no Next.js
-  images: { 
-    unoptimized: true 
-  } 
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
